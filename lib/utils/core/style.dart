0fa225d68f17a967b2293d460a6fe92db63d90ac
Eslamadelelkhouly <rodyerak@gmail.com>
2025-06-16 10:52:26 +0300

    add style font

---
+++ lib/utils/core/style.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Style {
+  static  TextStyle textStylebold50 = GoogleFonts.orbitron(
+    fontSize: 50,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+  static  TextStyle textStylemedium18 = GoogleFonts.orbitron(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+  static  TextStyle textStylemedium12 = GoogleFonts.orbitron(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+  static  TextStyle textStylemedium8 = GoogleFonts.orbitron(
+    fontSize: 8,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+  static  TextStyle textStylemedium15 = GoogleFonts.orbitron(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+}
